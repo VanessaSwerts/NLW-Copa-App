b3c035c00b359fbbc87878a73a05829ed62d7548
Vanessa Swerts <vanessaswerts@gec.inatel.br>
2022-11-06 19:22:36 -0300

    Finish screens layout and connection with server

--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -1,10 +1,65 @@
-import { Heading, VStack } from "native-base";
+import { useState } from 'react';
+import { Heading, VStack, useToast } from "native-base";
+import { useNavigation } from '@react-navigation/native';
 import { Header } from "../components/Header";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+import { api } from '../services/api'
+
 export function Find() {
+  const [code, setCode] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const { navigate } = useNavigation();
+
+  async function handleJoinPoll() {
+    if (!code.trim()) {
+      return toast.show({
+        title: 'Informe o código para buscar o bolão!',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
+
+    setIsLoading(true)
+
+    try {
+      await api.post('/polls/join', { code })
+
+      toast.show({
+        title: 'Você entrou no bolão com sucesso!',
+        placement: 'top',
+        bgColor: 'green.500'
+      })
+
+      navigate('polls')
+
+    } catch (error) {
+      console.log(error)
+      setCode('')
+      setIsLoading(false)
+
+      if (error.response?.data?.message === 'Poll not found.') {
+        return toast.show({
+          title: 'Não foi possivel encontrar o bolão!',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+
+      if (error.response?.data?.message === 'You already joined this poll.') {
+        return toast.show({
+          title: 'Você já faz parte desse bolão!',
+          placement: 'top',
+          bgColor: 'red.500'
+        })
+      }
+    }
+  }
+
+
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Buscar por código" showBackButton={true} />
@@ -14,9 +69,19 @@ export function Find() {
           Encontre um bolão através de {'\n'} seu código único!
         </Heading>
 
-        <Input mb={2} placeholder="Qual o código do bolão?" />
+        <Input
+          mb={2}
+          placeholder="Qual o código do bolão?"
+          autoCapitalize='characters'
+          value={code}
+          onChangeText={setCode}
+        />
 
-        <Button title="Buscar Bolão" />
+        <Button
+          title="Buscar Bolão"
+          isLoading={isLoading}
+          onPress={handleJoinPoll}
+        />
 
       </VStack>
 
--- src/screens/Polls.tsx
+++ src/screens/Polls.tsx
@@ -1,12 +1,43 @@
-import { VStack, Icon } from 'native-base';
+import { useCallback, useState } from 'react'
+import { VStack, Icon, useToast, FlatList } from 'native-base';
 import { Octicons } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
 import { Header } from '../components/Header';
 import { Button } from '../components/Button';
+import { Loading } from '../components/Loading'
+import { PollCard, PollCardsPros } from '../components/PollCard'
+import { EmptyPollList } from '../components/EmptyPollList'
+
+import { api } from '../services/api'
 
 export function Polls() {
   const { navigate } = useNavigation();
+  const [isLoading, setIsLoading] = useState(true);
+  const [polls, setPolls] = useState<PollCardsPros[]>([]);
+  const toast = useToast();
+
+  async function getPolls() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/polls')
+      setPolls(response.data.polls)
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possivel carregar os bolões!',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    getPolls()
+  }, []))
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -19,6 +50,20 @@ export function Polls() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {
+        isLoading
+          ? <Loading />
+          : <FlatList
+            data={polls}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <PollCard data={item} />}
+            ListEmptyComponent={() => <EmptyPollList />}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+          />
+      }
 
     </VStack>
   );
